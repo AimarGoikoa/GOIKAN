67ce93320e852b1e70a5f2e72350f0ae937ac3b5
alumMA <agoikoasan1@educacion.navarra.es>
2024-11-19 11:36:16 +0100

    Cabecera y css

--- includes/cabecera.php
+++ includes/cabecera.php
@@ -3,7 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../css/main.css">
+    <title>GOIKAN</title>
 </head>
 <body>
     
+    <header class="cabecera">
+        <div class="logo">
+            <a href="../index.php"><img src="../imagenes/goikan logo.png"></a>
+        </div>
+        <nav class="menu">
+            <button><a href="../pizzas.php">PIZZAS</a></button>
+            <button><a href="../nachos.php">NACHOS</a></button>
+        </nav>
+        <div class="nombre">
+            <p>GOIKAN</p>
+        </div>
+        <div class="opciones">
+            <button><a href="../login.php">INICIO DE SESIÓN/REGISTRO</a></button>
+            <button><a href="../config.php">CONFIGURACIÓN</a></button>
+        </div>
+        <div class="carrito">
+            <button><a href="../resumen.php">CARRITO</a></button>
+        </div>
+    </header>
+
