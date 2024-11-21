aacddecf559f59c83a47e9acca178c3ce86401a0
alumMA <agoikoasan1@educacion.navarra.es>
2024-11-21 10:55:24 +0100

    algunos cambios

--- index.php
+++ index.php
@@ -2,41 +2,46 @@
 include "cabecera.php";
 ?>
 
-<div class="gallery">
+    <div class="galeria">
+
+    <div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
-<div class="gallery">
+<div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
-<div class="gallery">
+<div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
-<div class="gallery">
+<div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
-<div class="gallery">
+<div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
-<div class="gallery">
+<div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
+
+
+    </div>
 
 <?php
 include "pie.php"
