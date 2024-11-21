d85f2b566d0a960af2be68c3fcd909d2c8a74f6c
alumMA <agoikoasan1@educacion.navarra.es>
2024-11-21 10:15:53 +0100

    Algunos cambios en css y galeria de imagenes (no terminada)

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include "cabecera.php";
+?>
+
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
+  </a>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
+  </a>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
+  </a>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
+  </a>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
+  </a>
+</div>
+
+<div class="gallery">
+  <a target="_blank" href="img_5terre.jpg">
+    <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
+  </a>
+</div>
+
+<?php
+include "pie.php"
+?>
---
+++ pie.php
@@ -0,0 +1,16 @@
+
+<footer class="pie">
+
+    <div class="menu">
+        <button type="button"> <a href="http://" target="_blank" rel="noopener noreferrer"> Lista de alergenos</a></button>
+        <button ><a href="resenas.php">Valoraciones</a></button>
+        <button><a href="contacto.php">Contacto</a></button>
+    </div>
+    
+    <p>2024 GOIKAN - Todos los dercechos reservados Contacto :info@goikan.com | Telefono 123-456-789</p>
+
+</footer>
+
+</body>
+
+</html>
