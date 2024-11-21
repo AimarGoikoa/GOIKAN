04d4ae12e1d7ae2ccdf6c37cabf35837b8b311e3
alumMA <agoikoasan1@educacion.navarra.es>
2024-11-21 10:29:04 +0100

    cambio en enlaces de las fotos del index

--- index.php
+++ index.php
@@ -3,37 +3,37 @@ include "cabecera.php";
 ?>
 
 <div class="gallery">
-  <a target="_blank" href="img_5terre.jpg">
+  <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="img_5terre.jpg">
+  <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="img_5terre.jpg">
+  <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="img_5terre.jpg">
+  <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="img_5terre.jpg">
+  <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
 
 <div class="gallery">
-  <a target="_blank" href="img_5terre.jpg">
+  <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
 </div>
