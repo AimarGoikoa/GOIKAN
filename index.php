7799eea770a284e7a42041c92657bb4add53702a
alumMA <agoikoasan1@educacion.navarra.es>
2024-11-26 10:53:56 +0100

    Index.php y CSS

--- index.php
+++ index.php
@@ -8,36 +8,42 @@ include "cabecera.php";
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
+  <div class="desc">DESCRIPCION DE LA OFERTA</div>
 </div>
 
 <div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
+  <div class="desc">DESCRIPCION DE LA OFERTA</div>
 </div>
 
 <div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
+  <div class="desc">DESCRIPCION DE LA OFERTA</div>
 </div>
 
 <div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
+  <div class="desc">DESCRIPCION DE LA OFERTA</div>
 </div>
 
 <div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
+  <div class="desc">DESCRIPCION DE LA OFERTA</div>
 </div>
 
 <div class="imagenes">
   <a target="_blank" href="pizzas.php">
     <img src="imagenes/images.jpg" alt="pizza1" width="350" height="200">
   </a>
+  <div class="desc">DESCRIPCION DE LA OFERTA</div>
 </div>
 
 
