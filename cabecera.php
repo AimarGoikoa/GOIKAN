a5b8bb460a639944a8ee7510088a29637c4418a9
alumMA <cmartinakh1@educacion.navarra.es>
2025-01-14 11:32:14 +0100

    para controlador

--- cabecera.php
+++ cabecera.php
@@ -13,15 +13,15 @@
             <a href="index.php"><img src="imagenes/goikan logo.png"></a>
         </div>
         <nav class="menu">
-            <button><a href="pizzas.php">PIZZAS</a></button>
-            <button><a href="nachos.php">NACHOS</a></button>
+            <button><a href="index.php?page=pizzas.php">PIZZAS</a></button>
+            <button><a href="index.php?page=nachos.php">NACHOS</a></button>
         </nav>
         <div class="opciones">
             <button><a href="index.php?page=login.php">INICIO DE SESIÓN/REGISTRO</a></button>
-            <button><a href="config.php">CONFIGURACIÓN</a></button>
+            <button><a href="index.php?page=config.php">CONFIGURACIÓN</a></button>
         </div>
         <div class="carrito">
-            <button><a href="resumen.php">CARRITO</a></button>
+            <button><a href="index.php?page=resumen.php">CARRITO</a></button>
         </div>
     </header>
 
--- pie.php
+++ pie.php
@@ -1,14 +1,14 @@
 <footer class="cabecera">
 
     <div class="menu">
-        <button><a href="resenas.php"> Lista de alergenos</a></button>
-        <button><a href="resenas.php">Valoraciones</a></button>
+        <button><a href="index.php?page=alergenos"> Lista de alergenos</a></button>
+        <button><a href="index.php?page=resenas.php">Valoraciones</a></button>
     </div>
     <div class="carrito">
         <p>2024 GOIKAN - Todos los dercechos reservados Contacto :info@goikan.com | Tlf 123-456-789</p>
     </div>
     <div class="menu">
-        <button><a href="contacto.php">Contacto</a></button>
+        <button><a href="index.php?page=contacto.php">Contacto</a></button>
     </div>
 
 
