e7445114895e38066f6e8e0921dada422e83fc5f
AngelA <angel.alonra@gmail.com>
2025-03-03 10:12:55 +0100

    actualizar

--- pie.php
+++ pie.php
@@ -1,19 +1,16 @@
-<footer class="cabecera">
 
+</main>
+<footer class="cabecera">
     <div class="menu">
-        <button><a href="https://www.lapiazzapizzeria.es/wp-content/uploads/2023/09/carta-alergenos-es.pdf"> Lista de alergenos</a></button>
+        <button><a href="https://www.lapiazzapizzeria.es/wp-content/uploads/2023/09/carta-alergenos-es.pdf">Lista de alérgenos</a></button>
         <button><a href="index.php?page=resenas.php">Valoraciones</a></button>
     </div>
     <div class="carrito">
-        <p>2024 GOIKAN - Todos los dercechos reservados Contacto :info@goikan.com | Tlf 123-456-789</p>
+        <p>2024 GOIKAN - Todos los derechos reservados | info@goikan.com | Tlf 123-456-789</p>
     </div>
     <div class="menu">
         <button><a href="index.php?page=contacto.php">Contacto</a></button>
     </div>
-
-
 </footer>
-
 </body>
-
 </html>
